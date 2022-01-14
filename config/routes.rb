62ca3c7cd845029734d390b82a3a6955a5e32111
lucasBarbeito <lucas.barbeito@ing.austral.edu.ar>
2022-01-14 18:28:22 -0300

    Add and configure Devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Hash symbol # in Ruby represents a method. As you remember an action is just a public method,
   # so pages# index says “call the PagesController and its public method (action) index.”
   root to: 'pages#index'
