5c295a652cd5778e1ad6e1025eb6b5e625cb1eec
lucasBarbeito <lucas.barbeito@ing.austral.edu.ar>
2022-01-14 19:19:05 -0300

    - Add the name column to the users table. - Include name attribute to sign_up_params and account_update_params   methods inside the RegistrationsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Hash symbol # in Ruby represents a method. As you remember an action is just a public method,
-  # so pages# index says “call the PagesController and its public method (action) index.”
+  devise_for :users, :controllers => {:registrations => "registrations"}
   root to: 'pages#index'
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2022_01_14_212736) do
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
