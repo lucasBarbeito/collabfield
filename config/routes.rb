771c2137a8f34056bb8800111f42905f6d32c060
lucasBarbeito <lucas.barbeito@ing.austral.edu.ar>
2022-01-14 19:00:18 -0300

    Change route from /users/sign_in to /login

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # Hash symbol # in Ruby represents a method. As you remember an action is just a public method,
   # so pages# index says “call the PagesController and its public method (action) index.”
   root to: 'pages#index'
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
 end
