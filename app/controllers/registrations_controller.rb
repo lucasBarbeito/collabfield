75e73de17bbee51c8a11cc3cef119f063de1c731
lucasBarbeito <lucas.barbeito@ing.austral.edu.ar>
2022-01-17 11:51:01 -0300

    Update the navigation bar - Add login, signup, logout and edit profile links on the navigation bar - Split _navigation.scss code into partials - Create responsive directory inside the stylesheets directory and add CSS. - Add CSS to tweak navigation bar style

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,11 +2,11 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
 
-  def sing_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def sign_up_params
+    params.require(:user).permit( :name, :email, :password, :password_confirmation)
   end
 
-   def account_update_params
-     params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
-   end
+  def account_update_params
+    params.require(:user).permit( :name, :email, :password, :password_confirmation, :current_password)
+  end
 end
