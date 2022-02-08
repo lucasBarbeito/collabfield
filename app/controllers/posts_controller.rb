ceda75650b1018df970b42b88c3ca238ebf074e1
lucasBarbeito <lucas.barbeito@ing.austral.edu.ar>
2022-02-08 11:15:18 -0300

    Create a UI to create new posts - Inside the PostsController:   define new and create actions   define a post_params method   define a before_action filter - Inside the ApplicationController:   define a redirect_if_not_signed_in method   define a redirect_if_signed_in method - Create a new template for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,25 @@
 class PostsController < ApplicationController
 
+  before_action :redirect_if_not_signed_in, only: [:new]
+
+
   def show
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      redirect_to root_path
+    end
   end
 
   def hobby
@@ -36,5 +54,10 @@ class PostsController < ApplicationController
        search: params[:search],
        category: params[:category],
        branch: params[:action]}).call
+  end
+
+  def post_params
+    #The permit method is used to whitelist attributes of the object, so only these specified attributes are allowed to be passed.
+    params.require(:post).permit(:content, :title, :category_id).merge(user_id: current_user.id)
   end
 end
