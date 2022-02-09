3427a75b089694ee257e8949b5879be5fad25624
lucasBarbeito <lucas.barbeito@ing.austral.edu.ar>
2022-02-09 12:21:31 -0300

    Add CSS to style the .contact-user form

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -43,7 +43,7 @@ module PostsHelper
 
   def contact_user_partial_path
     if user_signed_in?
-      @post.user.id != current_user.id ? 'posts/show/contact_user' : 'shared/empty_partial'
+      @post.user.id != current_user.id ? 'posts/show/contact_user' : 'posts/show/contact_user/yourself'
     else
       'posts/show/login_required'
     end
