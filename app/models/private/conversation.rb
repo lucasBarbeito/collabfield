d51b7698ce98038660459b4b3e72072cd0aade50
lucasBarbeito <lucas.barbeito@ing.austral.edu.ar>
2022-02-09 10:48:41 -0300

    Create Private::Conversation and Private::Message models - Define associations between User, Private::Conversation   and Private::Message models - Define private_conversations and private_messages tables

---
+++ app/models/private/conversation.rb
@@ -0,0 +1,14 @@
+class Private::Conversation < ApplicationRecord
+  self.table_name = 'private_conversations'
+  has_many :messages,
+           class_name: "Private::Message",
+           foreign_key: :conversation_id
+  belongs_to :sender, foreign_key: :sender_id, class_name: "User"
+  belongs_to :recipient, foreign_key: :recipient_id, class_name: "User"
+end
+
+# class_name Specify the class name of the association. Use it only if that name can't be inferred from the association name.
+# So belongs_to :sender will by default be linked to the Sender class, but if the real class name is User, you'll have to specify it with this option.
+
+# The foreign_key is used to specify a name of association’s column in a database table.
+# A data column in a table is only created on the belongs_to association’s side, but to make the column recognizable, we’ve to define the foreign_key with same values on both models.
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   #The dependent: :destroy argument says, when a user gets deleted, all posts what the user has created will be deleted too.
   has_many :posts, dependent: :destroy
+  has_many :private_messages, class_name:'Private::Message'
+  has_many :private_conversations, foreign_key: :sender_id, class_name: 'Private::Conversation'
 end
